3ca4430ea7aa70b332243c5569c2965c7d840bcd
Jhonatan Henrique <jhonatan.henrisilva@gmail.com>
2021-07-31 12:41:29 -0300

    Rotas de produtos

--- src/modules/products/services/ShowProductService.ts
+++ src/modules/products/services/ShowProductService.ts
@@ -14,7 +14,7 @@ class ShowProductService {
     const product = await productsRepository.findOne(id);
 
     if (!product) {
-      throw new AppError('Product not find');
+      throw new AppError('Product not found');
     }
 
     return product;
